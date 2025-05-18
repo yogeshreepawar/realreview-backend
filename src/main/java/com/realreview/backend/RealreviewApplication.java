f4985af8dcda76cb1e2f7550755010703356c927
yogeshreepawar <yogeshree2001@gmail.com>
2025-05-18 14:14:11 +0530

    backend

---
+++ src/main/java/com/realreview/backend/RealreviewApplication.java
@@ -0,0 +1,13 @@
+package com.realreview.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realreview/backend/RealreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realreview.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
